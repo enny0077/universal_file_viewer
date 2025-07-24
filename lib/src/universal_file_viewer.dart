ad413e85e9bed0c61f5958e0c2ba0bcf83d60732
enny0077 <eniola@tezda.com>
2025-07-24 12:02:21 +0100

    made some changes

--- lib/src/universal_file_viewer.dart
+++ lib/src/universal_file_viewer.dart
@@ -3,7 +3,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:pdfrx/pdfrx.dart';
+import 'package:pdfx/pdfx.dart';
 import 'package:universal_file_viewer/src/excel_csv_file_preview.dart';
 import 'package:universal_file_viewer/src/txt_file_preview.dart';
 import 'package:universal_file_viewer/src/word_file_preview.dart';
@@ -29,30 +29,54 @@ class UniversalFileViewer extends StatelessWidget {
   /// button is returned that opens the file in an external application when
   /// pressed.
   @override
+  @override
   Widget build(BuildContext context) {
     if (supportedFile(file.path)) {
       final fileType = detectFileType(file.path)!;
       switch (fileType) {
         case FileType.image:
           return Container(
-              padding: padding,
-              color: backgroundColor,
-              child: Center(child: Image.file(file)));
+            padding: padding,
+            color: backgroundColor,
+            child: Center(child: Image.file(file)),
+          );
+
         case FileType.video:
           return Container(
-              padding: padding,
-              color: backgroundColor,
-              child: VideoPlayerWidget(file: file));
+            padding: padding,
+            color: backgroundColor,
+            child: VideoPlayerWidget(file: file),
+          );
+
         case FileType.pdf:
-          return PdfViewer.file(file.path,
-              params: const PdfViewerParams(margin: 32));
+          final pdfController = PdfController(
+            document: PdfDocument.openFile(file.path),
+          );
+
+          return Container(
+            color: backgroundColor,
+            padding: padding,
+            child: PdfView(
+              controller: pdfController,
+              scrollDirection: Axis.vertical,
+              pageSnapping: true,
+              physics: const BouncingScrollPhysics(),
+              onDocumentError: (error) {
+                debugPrint('PDF error: $error');
+              },
+            ),
+          );
+
         case FileType.word:
           return DocxToFlutter(file: file, padding: padding);
+
         case FileType.excel:
         case FileType.csv:
           return ExcelCSVPreviewScreen(file: file, padding: padding);
+
         case FileType.text:
           return TxtPreviewScreen(file: file, padding: padding);
+
         case FileType.md:
           return MdPreviewScreen(file: file, padding: padding);
       }
